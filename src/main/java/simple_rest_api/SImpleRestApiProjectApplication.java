57483e2e23fbccab4ec4300ae8504b76f4c539fd
Peter <piotr.szaran@gmail.com>
2020-06-18 13:53:21 +0200

    v01 - Initial commit

---
+++ src/main/java/simple_rest_api/SImpleRestApiProjectApplication.java
@@ -0,0 +1,13 @@
+package simple_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SImpleRestApiProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SImpleRestApiProjectApplication.class, args);
+    }
+
+}
